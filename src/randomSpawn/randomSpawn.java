4e11990c2434f1a02728557b13083f006cac66e0
maxxie114 <maxxie114@gmail.com>
2018-07-28 01:17:03 -0700

    Fixed all known bugs, update to version 2.0.0

--- src/randomSpawn/randomSpawn.java
+++ src/randomSpawn/randomSpawn.java
@@ -2,41 +2,49 @@ package randomSpawn;
 /*
  * Description: This plugin randomly spawn players at different places with a range specified in config.yml
  * 
- * Version: 1.0.0
+ * Version: 2.0.0
  * 
  * Author: maxxie114
  */
 
 
-
+//import all dependencies from java libraries
 import java.io.File;
 import java.util.LinkedHashMap;
 
+//import all dependencies from nukkitx
+import cn.nukkit.level.Position;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
 import cn.nukkit.utils.TextFormat;
 
-
+//Create class
 public class randomSpawn extends PluginBase{
-	
+	//define variables
 	public int maxRange;
 	public String maxRangeInStr;
 	public int rnge;
+	public Position worldspawn;
 	
+	//When the plugin is loaded
 	@Override
 	public void onLoad() {
 		this.getLogger().info(TextFormat.GREEN + "randomSpawn is loaded!");
 		
 	}
 	
+	//Action to perform When the plugin is enabled
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onEnable() {
-		
+		//register EventListener so it will work
 		this .getServer().getPluginManager().registerEvents(new EventListener(this), this);
 		this.getLogger().info(TextFormat.YELLOW + "randomSpawn enabled!");
 		
+		//get the world spawn as a position object and save it in worldspawn
+		worldspawn = this.getServer().getDefaultLevel().getSpawnLocation();
 		
+		//Create config.yml
   		Config config = new Config(
 				new File(this.getDataFolder(), "config.yml"),
  				Config.YAML,
@@ -47,34 +55,37 @@ public class randomSpawn extends PluginBase{
 					 */
 					private static final long serialVersionUID = 1L;
 
-
+					//create key spawnRange with default input 900
 					{
 						put("spawnRange","900");
 					}
 				});
   		
-  		
+  		//get SpawnRange as a string and store it in maxRangeInStr
   		maxRangeInStr = String.valueOf(config.get("spawnRange"));
-  		config.save();
   		
+  		//save config file
+		config.save();
+  		
+		//set maxRange variable to the max range defined in config.yml
   		setMaxRange(getMaxRange(maxRangeInStr));
   		
 		
 	}
 	
 	
-	
+	//This method Convert String value rangeInStr to Int value
 	public int getMaxRange(String rangeInStr) {
 		int range = Integer.valueOf(rangeInStr);
 		return range;
 	}
-	
+	//This Set maxRange to the an int value 
 	public void setMaxRange(int range) {
 		maxRange = range;
 	}
 	
 	
-	
+	//Action to do when plugin is disabled
 	public void onDisable() {
 		this.getLogger().info(TextFormat.RED + "randomSpawn disabled!");
 	}
