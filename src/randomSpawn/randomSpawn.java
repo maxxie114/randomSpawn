c7dedef0f44c11311656fa13756fe09efd5b5772
maxxie114 <maxxie114@gmail.com>
2018-09-02 16:11:56 -0700

    Fixing most of the bugs
    Failed to writing changes into the config file

--- src/randomSpawn/randomSpawn.java
+++ src/randomSpawn/randomSpawn.java
@@ -10,7 +10,15 @@ package randomSpawn;
 
 //import all dependencies from java libraries
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
+//import java.util.Set;
+
+//import com.google.common.io.Files;
 
 //import all dependencies from nukkitx
 import cn.nukkit.level.Position;
@@ -20,32 +28,48 @@ import cn.nukkit.utils.TextFormat;
 
 //Create class
 public class randomSpawn extends PluginBase{
-	//define variables
+	//define public variables
 	public int maxRange;
 	public String maxRangeInStr;
 	public int rnge;
 	public Position worldspawn;
-	
+	public EventListener listener;
+	public Object playerList = new HashSet<String>(Arrays.asList(""));
+	//define private variables
+	private Config config;
+	private Config list;
 	//When the plugin is loaded
+	//private EventListener event; 
 	@Override
 	public void onLoad() {
 		this.getLogger().info(TextFormat.GREEN + "randomSpawn is loaded!");
 		
 	}
-	
-	//Action to perform When the plugin is enabled
-	@SuppressWarnings("deprecation")
+	//When the plugin is enabled
+	@SuppressWarnings({ "deprecation" })
 	@Override
 	public void onEnable() {
 		//register EventListener so it will work
-		this .getServer().getPluginManager().registerEvents(new EventListener(this), this);
+		this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
 		this.getLogger().info(TextFormat.YELLOW + "randomSpawn enabled!");
-		
-		//get the world spawn as a position object and save it in worldspawn
+		this.getLogger().info(TextFormat.RED + "Debug: randomSpawn3.0.0_Dev"); //test code
+		//this.getLogger().info(TextFormat.RED + "Debug: " + list.get("existingPlayers").toString());
+		//get the world spawn as a position object
 		worldspawn = this.getServer().getDefaultLevel().getSpawnLocation();
 		
+		//this.getLogger().info(String.valueOf(this.getDataFolder().mkdirs()));
+		
+//		this.getServer().getScheduler().scheduleRepeatingTask(new BroadcastPluginTask(this), 200);
+		
+//		this.saveResource("test.txt");
+		
+		//Create playerList.yml
+		this.list = new Config(new File(this.getDataFolder(), "playerList.yml"), Config.YAML);
+		
+		list.set("existingPlayers", playerList);
+		//config.save();
 		//Create config.yml
-  		Config config = new Config(
+  		this.config = new Config(
 				new File(this.getDataFolder(), "config.yml"),
  				Config.YAML,
  				
@@ -61,11 +85,17 @@ public class randomSpawn extends PluginBase{
 					}
 				});
   		
-  		//get SpawnRange as a string and store it in maxRangeInStr
+  		//get all info
   		maxRangeInStr = String.valueOf(config.get("spawnRange"));
+  		playerList = list.get("existingPlayers");
   		
+//  	this.getLogger().info(String.valueOf(getMaxRange(maxRangeInStr)));  //test code
+  		//this.getLogger().info("this is just a test");  //test code
   		//save config file
 		config.save();
+		list.save();
+		
+		this.getLogger().info(TextFormat.RED + "Debug: " + list.get("existingPlayers").toString()); //test code
   		
 		//set maxRange variable to the max range defined in config.yml
   		setMaxRange(getMaxRange(maxRangeInStr));
@@ -76,19 +106,32 @@ public class randomSpawn extends PluginBase{
 	
 	//This method Convert String value rangeInStr to Int value
 	public int getMaxRange(String rangeInStr) {
+		//Integer newInt = new Integer(maxRangeInStr);
 		int range = Integer.valueOf(rangeInStr);
 		return range;
 	}
-	//This Set maxRange to the an int value 
+	//This Set maxRange to the an Int value 
 	public void setMaxRange(int range) {
 		maxRange = range;
 	}
 	
+	//This function store a HashSet into a file
+	public void writePlayerList() {
+		//Create another yml file for existing player data
+		//list.set("existingPlayers", playerList);
+  		list.save();
+	}
 	
 	//Action to do when plugin is disabled
 	public void onDisable() {
 		this.getLogger().info(TextFormat.RED + "randomSpawn disabled!");
+		this.getLogger().info(TextFormat.RED + "Debug: " + playerList.toString()); //test code
+		writePlayerList();
 	}
 	
+	//just for compile in eclipse
+	public static void main(String[] args) {
+		System.out.println("compiled");
+	}
 
 }
